cc62990158b59a97dd29f38825dbfe1a2160bb85
kjames <kgrady1@email.arizona.edu>
2017-11-13 19:02:24 -0700

    adding more to the template

--- Drawing/Drawing.java
+++ Drawing/Drawing.java
@@ -18,7 +18,20 @@ public class Drawing extends Application {
 	
 	@Override
 	public void start(Stage pStage) throws Exception {
-	
+		int numBlocks = DEFAULT_SIZE;
+		
+		pStage.setTitle("Section 11");
+		BorderPane layout = new BorderPane();
+		Scene pScene = new Scene( layout, TILE_SIZE * ( numBlocks + 1 ), TILE_SIZE * numBlocks); 
+		
+		Canvas canvas = new Canvas(TILE_SIZE * numBlocks, TILE_SIZE * numBlocks);
+		pBoard = canvas.getGraphicsContext2D();
+		
+		layout.setLeft(cls);
+		layout.setCenter(canvas);
+		
+		pStage.setScene(pScene);
+		pStage.show();
 	}
 }
 
